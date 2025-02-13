5df563e2920c9e16adaafa70d141540c70c7dfc0
andrescallejas <andres.f.callejas@hotmail.es>
2025-02-12 19:42:57 -0500

    carga inicial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bigdata_actividad1",
+    version="0.0.1",
+    author="Andres Callejas",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "openpyxl"
+    ]
+    
+    
+)
